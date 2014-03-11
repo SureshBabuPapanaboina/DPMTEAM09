b02ec0c9f3afff142651019f2a2f037a7d9828e5
cyc115 <yuechuan20@gmail.com>
2014-03-11 12:11:14 -0400

    minor changes

---
+++ Lab6/src/other/Lab6Entry.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package other;
+
+import ultrasonicListeners.LocalizeLab5;
+
+/**
+ * @author yuechuan
+ *
+ */
+public class Lab6Entry {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
