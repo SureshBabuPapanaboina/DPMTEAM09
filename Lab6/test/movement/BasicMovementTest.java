8ab57418b77bdb45712fe23cbd965e834c0aec3b
Peter Henderson <Breakend@Peters-MacBook-Pro.local>
2014-03-12 23:26:15 -0400

    Added Init test folder

---
+++ Lab6/test/movement/BasicMovementTest.java
@@ -0,0 +1,17 @@
+package movement;
+
+/**
+ * Tests the basic movement and config parameters (wheel width/radius)
+ * 
+ *
+ */
+public class BasicMovementTest {
+
+	/** 
+	 * Move in a square and try to fix the config params until they're just right
+	 * @param args
+	 */
+	public static void main(String[] args){
+		
+	}
+}
