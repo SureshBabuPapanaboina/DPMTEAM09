7678653f54eea55377f0f306ec6ce210c78d2899
Peter Henderson <Breakend@Peters-MacBook-Pro.local>
2014-03-12 23:21:35 -0400

    Forgot about searching

---
+++ Lab6/src/search/Searcher.java
@@ -0,0 +1,30 @@
+package search;
+
+import lejos.robotics.pathfinding.Path;
+
+/**
+ * This generates a search path along the endzone and begins to search for the flag
+ *
+ */
+public class Searcher {
+
+	public Searcher(){
+		
+	}
+	
+	/**
+	 * Generates a search path based on the config
+	 */
+	public Path generateSearchPath(){
+		return null;
+		
+	}
+	
+	/**
+	 * Begins searching the path
+	 */
+	public void search(){
+		
+	}
+	
+}
