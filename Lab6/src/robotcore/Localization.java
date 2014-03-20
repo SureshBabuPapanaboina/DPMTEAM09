21d9a11ab751257e76346eac2fa684410b3a37ce
cyc115 <yuechuan20@gmail.com>
2014-03-19 20:37:12 -0400

    is localization on git?

--- Lab6/src/robotcore/Localization.java
+++ Lab6/src/robotcore/Localization.java
@@ -1,4 +1,7 @@
 package robotcore;
+
+import lejos.robotics.pathfinding.Path;
+
 /**
  * Does the localization, assuming all used threads are started.
  * @author yuechuan
@@ -8,16 +11,22 @@ public class Localization {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Path p = new Path();
 	}
 	/**
 	 * localization : first 
 	 */
 	public static void localize() {
 		prepareForFallingEdge();	
-		
+		performFallingEdge();
 	}
 	
+	/**
+	 * do falling edge and turn face the x axis 
+	 */
+	private static void performFallingEdge() {
+		
+	}
 	/**
 	 * make sure the robot is facing outward 
 	 */
