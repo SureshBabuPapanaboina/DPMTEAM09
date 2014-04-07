cd4d1c1d4b21cad45909a38b33f13c6b2a3228ea
bonan1 <wsshen1@gmail.com>
2014-04-06 21:06:00 -0400

    localization added

--- Lab6/src/robotcore/LocalizationIIII.java
+++ Lab6/src/robotcore/LocalizationIIII.java
@@ -3,6 +3,7 @@ package robotcore;
 import lejos.nxt.comm.RConsole;
 import movement.Driver;
 import odometry.Odometer;
+import odometry.OdometerCorrection;
 import sensors.LineReader;
 import sensors.LineReaderListener;
 import sensors.UltrasonicPoller;
@@ -13,6 +14,7 @@ public class LocalizationIIII{
 		private static Odometer odo = Odometer.getInstance();
 		private static UltrasonicPoller usp = UltrasonicPoller.getInstance();
 		private static Configuration conf = Configuration.getInstance();
+		private static OdometerCorrection odoC = OdometerCorrection.getInstance();
 		//private static LocalizationIII localization = new LocalizationIII();		//for subscribing to lin readers 
 		
 		private int distance1,distance2;
@@ -31,6 +33,7 @@ public class LocalizationIIII{
 			//RConsole.open();
 			init();
 			scan();
+			move();
 		}
 		
 		private static void init()
@@ -39,6 +42,8 @@ public class LocalizationIIII{
 			lcd.start();
 			odo.start();
 			usp.start();
+			LineReader.getLeftSensor().start();
+			LineReader.getRightSensor().start();
 			//LineReader.getLeftSensor().start();
 			//LineReader.getRightSensor().start();
 		}
@@ -48,22 +53,42 @@ public class LocalizationIIII{
 			switch(startCorner)
 			{
 				case LOW_LEFT:
-					conf.getCurrentLocation().setTheta(0).setX(15).setY(15);
+					conf.getCurrentLocation().setTheta(Math.PI/2).setX(-15).setY(-15);
 					break;
 				case LOW_RIGHT:
-					conf.getCurrentLocation().setTheta(270).setX(285).setY(15);
+					conf.getCurrentLocation().setTheta(0).setX(315).setY(-15);
 					break;
 				case UP_LEFT:
-					conf.getCurrentLocation().setTheta(90).setX(15).setY(285);
+					conf.getCurrentLocation().setTheta(Math.PI).setX(-15).setY(315);
 					break;
 				case UP_RIGHT :
-					conf.getCurrentLocation().setTheta(180).setX(285).setY(285);
+					conf.getCurrentLocation().setTheta(3*Math.PI/2).setX(315).setY(315);
 					break;
 			}
 		}
+		/*
+		private static void startAngle(int startCorner)
+		{
+			switch(startCorner)
+			{
+				case LOW_LEFT:
+					conf.getCurrentLocation().setTheta(90);
+					break;
+				case LOW_RIGHT:
+					conf.getCurrentLocation().setTheta(0);
+					break;
+				case UP_LEFT:
+					conf.getCurrentLocation().setTheta(180);
+					break;
+				case UP_RIGHT :
+					conf.getCurrentLocation().setTheta(270);
+					break;
+			}
+		}*/
 		
 		private static void scan()
 		{
+			//start(0);
 			double angle;
 			if(usp.getFilteredDistance()>50)
 			{
@@ -91,8 +116,19 @@ public class LocalizationIIII{
 					}
 				}
 			}
-			conf.getCurrentLocation().setTheta(Math.toRadians(90)).setX(0).setY(0);
-			//start();
-			driver.motorStop();
+			start(0);
+			//driver.motorStop();
+		}
+		
+		public static void move()
+		{
+			//LineReader.subscribeToAll(odoC);
+			driver.forward(28);
+			driver.turnTo(-90);
+			driver.forward(28);
+			while(true)
+			{
+				driver.motorStop();
+			}
 		}
 }
